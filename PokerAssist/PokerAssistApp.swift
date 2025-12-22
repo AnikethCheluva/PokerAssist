799528af5da7f557a420b2c26dc2fbacd7a0821e
AnikethCheluva <ani.cheluva@gmail.com>
2025-12-22 12:24:45 -0800

    Initial Commit

---
+++ PokerAssist/PokerAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerAssistApp.swift
+//  PokerAssist
+//
+//  Created by Aniketh Cheluva on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PokerAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
